160dc39f8bf6b5924ddfb064b1510865f4ee204c
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-01-19 07:45:59 -0500

    Removed the tracking code, now have a constant rectangle. Already kinda started on plotting an fft

--- python/mosse.py
+++ python/mosse.py
@@ -36,116 +36,6 @@ NUM_MEASURES = 100
 MA = 30
 RECENT_MA = 10
 
-def rnd_warp(a):
-    h, w = a.shape[:2]
-    T = np.zeros((2, 3))
-    coef = 0.2
-    ang = (np.random.rand()-0.5)*coef
-    c, s = np.cos(ang), np.sin(ang)
-    T[:2, :2] = [[c,-s], [s, c]]
-    T[:2, :2] += (np.random.rand(2, 2) - 0.5)*coef
-    c = (w/2, h/2)
-    T[:,2] = c - np.dot(T[:2, :2], c)
-    return cv2.warpAffine(a, T, (w, h), borderMode = cv2.BORDER_REFLECT)
-
-def divSpec(A, B):
-    Ar, Ai = A[...,0], A[...,1]
-    Br, Bi = B[...,0], B[...,1]
-    C = (Ar+1j*Ai)/(Br+1j*Bi)
-    C = np.dstack([np.real(C), np.imag(C)]).copy()
-    return C
-
-eps = 1e-5
-
-class MOSSE:
-    def __init__(self, frame, rect):
-        x1, y1, x2, y2 = rect
-        w, h = map(cv2.getOptimalDFTSize, [x2-x1, y2-y1])
-        x1, y1 = (x1+x2-w)//2, (y1+y2-h)//2
-        self.pos = x, y = x1+0.5*(w-1), y1+0.5*(h-1)
-        self.size = w, h
-        img = cv2.getRectSubPix(frame, (w, h), (x, y))
-
-        self.win = cv2.createHanningWindow((w, h), cv2.CV_32F)
-        g = np.zeros((h, w), np.float32)
-        g[h//2, w//2] = 1
-        g = cv2.GaussianBlur(g, (-1, -1), 2.0)
-        g /= g.max()
-
-        self.G = cv2.dft(g, flags=cv2.DFT_COMPLEX_OUTPUT)
-        self.H1 = np.zeros_like(self.G)
-        self.H2 = np.zeros_like(self.G)
-        for i in xrange(128):
-            a = self.preprocess(rnd_warp(img))
-            A = cv2.dft(a, flags=cv2.DFT_COMPLEX_OUTPUT)
-            self.H1 += cv2.mulSpectrums(self.G, A, 0, conjB=True)
-            self.H2 += cv2.mulSpectrums(     A, A, 0, conjB=True)
-        self.update_kernel()
-        self.update(frame)
-
-    def update(self, frame, rate = 0.125):
-        (x, y), (w, h) = self.pos, self.size
-        self.last_img = img = cv2.getRectSubPix(frame, (w, h), (x, y))
-        img = self.preprocess(img)
-        self.last_resp, (dx, dy), self.psr = self.correlate(img)
-        self.good = self.psr > 8.0
-        if not self.good:
-            return
-
-        self.pos = x+dx, y+dy
-        self.last_img = img = cv2.getRectSubPix(frame, (w, h), self.pos)
-        img = self.preprocess(img)
-
-        A = cv2.dft(img, flags=cv2.DFT_COMPLEX_OUTPUT)
-        H1 = cv2.mulSpectrums(self.G, A, 0, conjB=True)
-        H2 = cv2.mulSpectrums(     A, A, 0, conjB=True)
-        self.H1 = self.H1 * (1.0-rate) + H1 * rate
-        self.H2 = self.H2 * (1.0-rate) + H2 * rate
-        self.update_kernel()
-
-    @property
-    def state_vis(self):
-        f = cv2.idft(self.H, flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT )
-        h, w = f.shape
-        f = np.roll(f, -h//2, 0)
-        f = np.roll(f, -w//2, 1)
-        kernel = np.uint8( (f-f.min()) / f.ptp()*255 )
-        resp = self.last_resp
-        resp = np.uint8(np.clip(resp/resp.max(), 0, 1)*255)
-        vis = np.hstack([self.last_img, kernel, resp])
-        return vis
-
-    def draw_state(self, vis):
-        (x, y), (w, h) = self.pos, self.size
-        x1, y1, x2, y2 = int(x-0.5*w), int(y-0.5*h), int(x+0.5*w), int(y+0.5*h)
-        cv2.rectangle(vis, (x1, y1), (x2, y2), (0, 0, 255))
-        if self.good:
-            cv2.circle(vis, (int(x), int(y)), 2, (0, 0, 255), -1)
-        else:
-            cv2.line(vis, (x1, y1), (x2, y2), (0, 0, 255))
-            cv2.line(vis, (x2, y1), (x1, y2), (0, 0, 255))
-        draw_str(vis, (x1, y2+16), 'PSR: %.2f' % self.psr)
-
-    def preprocess(self, img):
-        img = np.log(np.float32(img)+1.0)
-        img = (img-img.mean()) / (img.std()+eps)
-        return img*self.win
-
-    def correlate(self, img):
-        C = cv2.mulSpectrums(cv2.dft(img, flags=cv2.DFT_COMPLEX_OUTPUT), self.H, 0, conjB=True)
-        resp = cv2.idft(C, flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
-        h, w = resp.shape
-        _, mval, _, (mx, my) = cv2.minMaxLoc(resp)
-        side_resp = resp.copy()
-        cv2.rectangle(side_resp, (mx-5, my-5), (mx+5, my+5), 0, -1)
-        smean, sstd = side_resp.mean(), side_resp.std()
-        psr = (mval-smean) / (sstd+eps)
-        return resp, (mx-w//2, my-h//2), psr
-
-    def update_kernel(self):
-        self.H = divSpec(self.H1, self.H2)
-        self.H[...,1] *= -1
-
 class Temple:
     def __init__(self, frame):
         self.h , self.w = frame.shape[:2]
@@ -193,7 +83,15 @@ class App:
         self.cap = video.create_capture(video_src)
         _, self.frame = self.cap.read()
         cv2.imshow('frame', self.frame)
-        self.rect_sel = RectSelector('frame', self.onrect)
+        h , w = self.frame.shape[:2]
+        self.roiw = w/3.0
+        self.roih = h/3.0
+        self.roix, self.roiy = (w/2.0, h - self.roih/2.0)
+        print "roix is" + str(self.roix)
+        print "roiy is" + str(self.roiy)
+        print "roiw is" + str(self.roiw)
+        print "roih is" + str(self.roih)
+
         self.trackers = []
         self.paused = paused
         self.temple = None
@@ -207,12 +105,7 @@ class App:
         self.recent_readings = np.zeros(RECENT_MA)
         self.recent_sum = 0
 
-    def onrect(self, rect):
-        frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        tracker = MOSSE(frame_gray, rect)
-        self.trackers.append(tracker)
         self.temple = Temple(self.frame)
-        print "new rect."
 
     def run(self):
         while True:
@@ -223,66 +116,70 @@ class App:
                     break
                 frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
-                if self.trackers and self.temple:
+                if self.temple:
                     final = self.frame.copy()
                     reading_no = self.frames_read % NUM_MEASURES
                     #print "Reading number " + str(reading_no)
                     self.temple.update(self.frame)
                     #cv2.imshow('frame', self.trackers[0].pos)
-                    for tracker in self.trackers:
-                      (x, y), (w, h) = tracker.pos, tracker.size
-                      tracker.update(frame_gray)
-                      tracker.draw_state(self.temple.vis)
-                      y_offset= int(h/2)
-                      x_offset= int(w/2)
-                      final[y-y_offset:y+y_offset, x-x_offset:x+x_offset] = self.temple.vis[y-y_offset:y+y_offset, x-x_offset:x+x_offset]
-                      sumDiff = cv2.sumElems(self.temple.real_diff[y-y_offset:y+y_offset, x-x_offset:x+x_offset])[0]
+                    x = self.roix
+                    y = self.roiy
+                    h = self.roih
+                    w = self.roiw
 
-                      reading = sumDiff/(w*h+1)
+                    y_offset= int(h/2)
+                    x_offset= int(w/2)
+                    final[y-y_offset:y+y_offset, x-x_offset:x+x_offset] = self.temple.vis[y-y_offset:y+y_offset, x-x_offset:x+x_offset]
+                    sumDiff = cv2.sumElems(self.temple.real_diff[y-y_offset:y+y_offset, x-x_offset:x+x_offset])[0]
+                    if sumDiff == 0:
+                      continue
 
-                      # For 'speed' we just subtract the value we're overwriting and then add the new one. The others don't change:
+                    reading = sumDiff/(w*h+1)
 
-                      #Moving average:
-                      ma_no = self.frames_read % MA
-                      self.sum_readings = self.sum_readings - self.readings[ma_no] + reading
-                      self.readings[ma_no] = reading
+                    # For 'speed' we just subtract the value we're overwriting and then add the new one. The others don't change:
 
-                      
-                      #short ma
-                      recent_ma_no = self.frames_read % RECENT_MA
-                      self.recent_sum = self.recent_sum - self.recent_readings[recent_ma_no] + reading
-                      self.recent_readings[recent_ma_no] = reading
+                    #Moving average:
+                    ma_no = self.frames_read % MA
+                    self.sum_readings = self.sum_readings - self.readings[ma_no] + reading
+                    self.readings[ma_no] = reading
 
-                      #print self.readings
-                      self.filtered[0][reading_no] = clock()
-                      self.filtered[1][reading_no] = self.recent_sum/RECENT_MA - self.sum_readings/MA #Remove long term noise
+                    
+                    #short ma
+                    recent_ma_no = self.frames_read % RECENT_MA
+                    self.recent_sum = self.recent_sum - self.recent_readings[recent_ma_no] + reading
+                    self.recent_readings[recent_ma_no] = reading
+
+                    #print self.readings
+                    self.filtered[0][reading_no] = clock()
+                    self.filtered[1][reading_no] = self.recent_sum/RECENT_MA - self.sum_readings/MA #Remove long term noise
 
                     self.frame = final.copy()
                     if reading_no == NUM_MEASURES-1: #When we've read a whole array in, flush it to a graph
+                      t = scipy.linspace(filtered[0][0],0.1995,filtered[0][NUM_MEASURES-1])
+
+                      FFT = abs(scipy.fft(self.readings))
+                      freqs = scipy.fftpack.fftfreq(readings.size, 0.1995)
+
+                      pylab.subplot(211)
+                      plt.plot(self.filtered[0], self.filtered[1])
+                      pylab.subplot(212)
+                      pylab.plot(freqs,20*scipy.log10(FFT),'x')
+                      pylab.show()
                       print "Saving pic.png"
                       plt.clf()
-                      plt.plot(self.filtered[0], self.filtered[1])
+                      #plt.plot(self.filtered[0], self.filtered[1])
                       plt.xlabel('time (s)')
-                      plt.ylabel('Chest Movement')
+                      plt.ylabel('Movement')
                       #grid(True)
                       plt.savefig("pic.png")
-                self.rect_sel.draw(self.frame)
                 cv2.imshow('frame', self.frame)
 
-            #if self.trackers:
-            #  print self.trackers[0].pos
-            if len(self.trackers) > 0:
-                cv2.imshow('tracker state', self.trackers[-1].state_vis)
 
-            #cv2.imshow('frame', self.temple.vis) #Show the part that is being tracked in diff mode.
             ch = cv2.waitKey(10)
             if ch == 27:
                 break
             if ch == ord(' '):
                 self.paused = not self.paused
-            if ch == ord('c'):
-                self.trackers = []
-
 
 if __name__ == '__main__':
     print __doc__
